bfefc209278e74e9f0ea732a4b9c8a62a0c4662d
Chuhan Lin <chuhanlin.business@gmail.com>
2023-09-19 18:43:23 -0400

    changed coverage to nearest integer

--- src/testing_custom_output/custom_test_output.ts
+++ src/testing_custom_output/custom_test_output.ts
@@ -7,31 +7,34 @@
 */
 
 import * as fs from 'fs';
+function print_test_results() {
+    // Read the contents of the output.txt file
+    const outputText = fs.readFileSync('src/testing_custom_output/output.txt', 'utf8');
 
-// Read the contents of the output.txt file
-const outputText = fs.readFileSync('src/testing_custom_output/output.txt', 'utf8');
+    //Regex to find total num of test cases and num of passed test cases and code coverage
+    const coverageRegex = /All files\s+\|\s+([\d.]+)\s+\|\s+([\d.]+)\s+\|\s+([\d.]+)\s+\|\s+([\d.]+)\s+\|/;
+    const testRegex = /Tests:\s+(\d+)\s*(?:failed,\s+(\d+)\s+)?passed,\s+(\d+)\s+total/;
 
-//Regex to find total num of test cases and num of passed test cases and code coverage
-const coverageRegex = /All files\s+\|\s+([\d.]+)\s+\|\s+([\d.]+)\s+\|\s+([\d.]+)\s+\|\s+([\d.]+)\s+\|/;
-const testRegex = /Tests:\s+(\d+)\s*(?:failed,\s+(\d+)\s+)?passed,\s+(\d+)\s+total/;
+    const coverageMatch = outputText.match(coverageRegex);
+    const testMatch = outputText.match(testRegex);
 
-const coverageMatch = outputText.match(coverageRegex);
-const testMatch = outputText.match(testRegex);
+    if (coverageMatch && testMatch) {
+        var [, stmts, branches, funcs, lines] = coverageMatch;
+        var [, testsFailed, testsPassed, testsTotal] = testMatch;
 
-if (coverageMatch && testMatch) {
-    var [, stmts, branches, funcs, lines] = coverageMatch;
-    var [, testsFailed, testsPassed, testsTotal] = testMatch;
-
-    /*Check if there are any failed test cases because when there are no failed test cases
-    there are 1 less variable to assign*/
-    if(!testsPassed){
-        testsPassed = testsFailed;
+        /*Check if there are any failed test cases because when there are no failed test cases
+        there are 1 less variable to assign*/
+        if(!testsPassed){
+            testsPassed = testsFailed;
+        }
+        console.log(`Total: ${testsTotal}`);
+        console.log(`Passed: ${testsPassed}`);
+        console.log(`Coverage: ${Math.round(parseFloat(lines))}%`);
+        console.log(`${testsPassed}/${testsTotal} test cases passed. ${Math.round(parseFloat(lines))}% line coverage achieved.`)
+    } else {
+        //Exit on error if there are no regex matched expression
+        process.exit(1);
     }
-    console.log(`Total: ${testsTotal}`);
-    console.log(`Passed: ${testsPassed}`);
-    console.log(`Coverage: ${lines}%`);
-    console.log(`${testsPassed}/${testsTotal} test cases passed. ${lines}% line coverage achieved.`)
-} else {
-    //Exit on error if there are no regex matched expression
-    process.exit(1);
 }
+
+print_test_results();
