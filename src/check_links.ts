f801e9199b6763f4bed1527a31b8ee6cd7dab984
psankar193 <pran804@gmail.com>
2023-09-20 00:45:52 -0400

    fixed

--- src/check_links.ts
+++ src/check_links.ts
@@ -1,15 +1,17 @@
 function isNpmLink(input: string): boolean {
 
     const pattern = /^https:\/\/www\.npmjs\.com\/package\/[a-zA-Z]+$/;
+    const pattern2 = /^npmjs\.com\/package\/[a-zA-Z]+$/;
   
   
-    return pattern.test(input);
+    return pattern.test(input) || pattern2.test(input);
   }
   
   function isGithubLink(input: string): boolean {
   
     const pattern = /^https:\/\/github\.com\/[a-zA-Z0-9\-]+\/[a-zA-Z0-9\-]+$/;
+    const pattern2 = /^github\.com\/[a-zA-Z0-9\-]+\/[a-zA-Z0-9\-]+$/;
   
   
-    return pattern.test(input);
+    return pattern.test(input) || pattern2.test(input);
   }
