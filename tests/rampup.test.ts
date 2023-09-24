8a4a05b279e10b18dfbc75f8ee5cad36a2dda0d3
psankar193 <pran804@gmail.com>
2023-09-24 14:48:25 -0400

    RampUp tests completed

---
+++ tests/rampup.test.ts
@@ -0,0 +1,54 @@
+import { RampUp } from "../src/rampup";
+import { Metric } from "../src/metric";
+
+describe('RampUpTest', () => {
+    // Test case 1
+    it('good link', async () => {
+        const RampUpInstance =  new RampUp("https://github.com/thockin/test");
+        await RampUpInstance.getGitHubRepoUrl("https://github.com/thockin/test");
+    
+        await RampUpInstance.cloneRepository();
+        const output = await RampUpInstance.rampup();
+        await RampUpInstance.deleteRepository();
+            
+        
+        expect(output).toBe(0);
+        
+
+      
+    });
+
+    it('fake_link', async () => {
+        const RampUpInstance =  new RampUp("https://github.com/Fake/Fake");
+        await RampUpInstance.getGitHubRepoUrl("https://github.com/Fake/Fake");
+    
+        await RampUpInstance.cloneRepository();
+        const output = await RampUpInstance.rampup();
+        await RampUpInstance.deleteRepository();
+
+        expect(output).toBe(-1);
+        
+        
+      
+    });
+
+    it('weaker link', async () => {
+        const RampUpInstance =  new RampUp("https://github.com/octokit/plugin-enterprise-server.js");
+        await RampUpInstance.getGitHubRepoUrl("https://github.com/octokit/plugin-enterprise-server.js");
+    
+        await RampUpInstance.cloneRepository();
+        const output = await RampUpInstance.rampup();
+            
+        await RampUpInstance.deleteRepository();
+        expect(output).toBe(0);
+        
+      
+    },20000);
+  
+});
+
+
+
+
+
+
