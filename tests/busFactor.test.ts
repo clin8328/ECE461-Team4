6bf88b2b5d0c49799b4adf8439077cb7f80ebd88
Chuhan Lin <chuhanlin.business@gmail.com>
2023-09-24 13:22:47 -0400

    push

--- tests/busFactor.test.ts
+++ tests/busFactor.test.ts
@@ -5,7 +5,7 @@ import { Metric } from '../src/metric';
 test('test_1 Bus Factor', async () => {
     let url = 'https://github.com/davisjam/safe-regex';
     let bus = new Bus(url);
-    const output = await bus.Bus_Factor();
+    const output = await bus.Bus_Factor(url);
     
     expect(output).toBe(0.4); // Corrected the usage of toBe()
 });
@@ -14,7 +14,7 @@ test('test_2 Bus Factor', async () => {
     let url = 'https://github.com/cloudinary/cloudinary_npm';
     let metric = new Metric(url, "test");
     let bus = new Bus(url);
-    const output = await bus.Bus_Factor();
+    const output = await bus.Bus_Factor(url);
     
     expect(output).toBe(0.2); // Corrected the usage of toBe()
 });
@@ -22,7 +22,7 @@ test('test_2 Bus Factor', async () => {
 test('fake github', async () => {
     let url = 'https://github.com/cloudinory/fakerepo';
     let bus = new Bus(url);
-    const output = await bus.Bus_Factor();
+    const output = await bus.Bus_Factor(url);
     
     expect(output).toBe(-1); // Corrected the usage of toBe()
 });
