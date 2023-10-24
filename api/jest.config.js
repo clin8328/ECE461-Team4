93283a5e07898c32dce0a698c906d7e183f660e4
davidasousa <122298257+davidasousa@users.noreply.github.com>
2023-10-24 15:24:06 -0400

    Delete api/jest.config.js

--- api/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true,
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
-};
-exports.default = config;
