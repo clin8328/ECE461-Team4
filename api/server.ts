ce9fce54a6da28ad3b7af64d3a40c76bffbbc6b4
TenGui <cyrus.z.xu@gmail.com>
2023-11-06 11:32:30 -0500

    fix capitalization

--- api/server.ts
+++ api/server.ts
@@ -9,7 +9,7 @@ import ResetRegistry from "./endpoints/resetregistry";
 import authenticate from "./endpoints/authenticate";
 import packages from "./endpoints/getAllPkg";
 import packageById from "./endpoints/getPkgById";
-import updatePkgById from "./endpoints/updatepkgById";
+import updatePkgById from "./endpoints/updatePkgById";
 import deletePkgById from "./endpoints/deletePkgById";
 import uploadPackage from "./endpoints/uploadPkg";
 import ratePkgById from "./endpoints/ratePkgById";
@@ -17,21 +17,18 @@ import getPkgByName from "./endpoints/getPkgByName";
 import deletePkgByName from "./endpoints/deletePkgByName";
 import getPackageByRegex from "./endpoints/getPkgByRegex";
 
-app.post("/packages", packages)
-app.put('/authenticate', authenticate);
-app.delete('/reset', ResetRegistry);
-app.get('/package/:id', packageById);
-app.put('/package/:id', updatePkgById);
-app.delete('/package/:id', deletePkgById);
-app.post('/package', uploadPackage);
-app.get('/package/:id/rate', ratePkgById);
-app.get('/package/byName/:name', getPkgByName);
-app.delete('/package/byName/:name', deletePkgByName);
-app.post('/package/byRegex', getPackageByRegex);
-
-
+app.post("/packages", packages);
+app.put("/authenticate", authenticate);
+app.delete("/reset", ResetRegistry);
+app.get("/package/:id", packageById);
+app.put("/package/:id", updatePkgById);
+app.delete("/package/:id", deletePkgById);
+app.post("/package", uploadPackage);
+app.get("/package/:id/rate", ratePkgById);
+app.get("/package/byName/:name", getPkgByName);
+app.delete("/package/byName/:name", deletePkgByName);
+app.post("/package/byRegex", getPackageByRegex);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
