4c75dc2d3be7b39620a09c5b530f71d6d7950bcd
Caden <cadenbrennan@gmail.com>
2023-11-08 15:20:20 -0500

    Delete Endpoints implementation.

--- api/endpoints/deletePkgById.ts
+++ api/endpoints/deletePkgById.ts
@@ -1,8 +1,24 @@
 import express, { Request, Response } from "express";
 import { query } from "../database";
 import jwt from "jsonwebtoken";
-function deletePkgById(req: Request, res: Response) {
-    return res.sendStatus(200);
+async function deletePkgById(req: Request, res: Response) {
+    const packageId = req.params.id;
+
+    try {
+        const result = await query("DELETE FROM packages WHERE id = $1 RETURNING *", [packageId]);
+
+        if (result.rowCount === 0) {
+            // No package found with the provided ID.
+            return res.status(404).send("Package not found");
+        }
+
+        // Send the deleted package data as a JSON response.
+        return res.status(200).json(result.rows[0]);
+    } catch (error) {
+        // Handle any potential errors during the database query.
+        console.error("Error deleting package by ID: ", error);
+        return res.status(500).send("Internal Server Error");
+    }
 }
 
   
--- api/endpoints/deletePkgByName.ts
+++ api/endpoints/deletePkgByName.ts
@@ -2,8 +2,24 @@ import express, { Request, Response } from "express";
 import { query } from "../database";
 import jwt from "jsonwebtoken";
 import { get } from "http";
-function deletePkgByName(req: Request, res: Response) {
-    return res.sendStatus(200);
+async function deletePkgByName(req: Request, res: Response) {
+    const packageName = req.params.byName;
+
+    try {
+        const result = await query("DELETE FROM packages WHERE name = $1 RETURNING *", [packageName]);
+
+        if (result.rowCount === 0) {
+            // No package found with the provided name.
+            return res.status(404).send("Package not found");
+        }
+
+        // Send the deleted package data as a JSON response.
+        return res.status(200).json(result.rows[0]);
+    } catch (error) {
+        // Handle any potential errors during the database query.
+        console.error("Error deleting package by name: ", error);
+        return res.status(500).send("Internal Server Error");
+    }
 }
 
   
