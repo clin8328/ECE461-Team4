2d479ece33553e8187d754b8fda08bb774a36089
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-12-01 18:29:36 -0500

    update script

--- getSecret.sh
+++ getSecret.sh
@@ -1,9 +1,12 @@
+# Install jq
+sudo yum install jq -y
+
 # Run the AWS CLI command to get the secret value
 secret_value=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-2:777501491721:secret:EN_content-UcMszU --query SecretString --output text)
 
-# Parse JSON string using bash
-GITHUB_TOKEN=$(echo "$secret_value" | sed -n 's/.*"GITHUB_TOKEN": "\(.*\)".*/\1/p')
-LOG_FILE=$(echo "$secret_value" | sed -n 's/.*"LOG_FILE": "\(.*\)".*/\1/p')
+# Parse JSON string using jq
+GITHUB_TOKEN=$(echo "$secret_value" | jq -r '.GITHUB_TOKEN')
+LOG_FILE=$(echo "$secret_value" | jq -r '.LOG_FILE')
 
 # Create the .env file with the extracted values
 echo "GITHUB_TOKEN='$GITHUB_TOKEN'" > ./.env
