2f085c67a0da077449ebddb0df9255ee5e8e740b
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-12-02 17:51:14 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
-pm2 delete 461Server
+# Check if the process is running before attempting to delete
+if pm2 pid 461Server > /dev/null 2>&1; then
+    pm2 delete 461Server
+fi
 
+# Start your application using PM2 with the updated configuration
 pm2 start ./api/pm2.config.json
 
+# Save the PM2 process list to restart on server reboot
 pm2 save
 
+# Display logs (optional, useful for debugging)
 pm2 logs 461Server
