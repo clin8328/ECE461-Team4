05b93cc65491cb75e20648a10435a927de088c4b
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-12-05 21:35:00 -0500

    fix_authentication

--- api/endpoints/authenticate.ts
+++ api/endpoints/authenticate.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from "express";
 import { query } from "../database";
 import jwt from "jsonwebtoken";
 const tokenKey: string = "461_secret_key";
-
+const defaultUsername = 'ece30861defaultadminuser';
+const defaultPassword = 'correcthorsebatterystaple123(!__+@**(A\'"`;DROP TABLE packages;';
 async function authenticate(req: Request, res: Response) {
   const request = req.body;
   if (request.User === undefined || request.Secret === undefined) {
@@ -13,9 +14,15 @@ async function authenticate(req: Request, res: Response) {
   }
   const username = request.User.name;
   const password = request.Secret.password;
-  const user = await query("SELECT * FROM users WHERE user_name = $1", [username]);
-  if (user.rows.length === 0 || user.rows[0].user_pass != password) {
-    return res.sendStatus(401);
+  if (username == defaultUsername) {
+      if (password != defaultPassword) {
+        return res.sendStatus(401);
+      }
+  } else {
+    const user = await query("SELECT * FROM users WHERE user_name = $1", [username]);
+    if (user.rows.length === 0 || user.rows[0].user_pass != password) {
+      return res.sendStatus(401);
+    }
   }
   const user_credential = {username: username, password: password};
   const token = jwt.sign(user_credential, tokenKey, { expiresIn: '10h' });
