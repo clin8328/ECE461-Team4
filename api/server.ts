6a2c0bf890e026ade4b899d4f0d65c02b3804ceb
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-12-05 22:07:22 -0500

    logger

--- api/server.ts
+++ api/server.ts
@@ -1,11 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
 import { query } from "./database";
+import winston from "winston";
 const app = express();
 const path = require('path');
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(express.static(path.join(__dirname, 'static')))
+const logfilepath = path.join(__dirname, 'log', 'logfile.log');
+const logFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.simple()
+);
+const logger = winston.createLogger({
+  level: 'info',
+  format: logFormat,
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: logfilepath }) // Log to a file
+  ],
+});
 //endpoints
 import ResetRegistry from "./endpoints/resetregistry";
 import authenticate from "./endpoints/authenticate";
@@ -19,6 +33,11 @@ import getPkgByName from "./endpoints/getPkgByName";
 import deletePkgByName from "./endpoints/deletePkgByName";
 import getPackageByRegex from "./endpoints/getPkgByRegex";
 
+app.use((req, res, next) => {
+  // Log the request information
+  logger.info(`${req.method} ${req.url} | Request Headers: ${JSON.stringify(req.headers)}${(req.method === 'POST' || req.method === 'PUT') ? ` | Request Body: ${JSON.stringify(req.body)}` : ''}`);
+  next();
+});
 
 app.post("/packages", packages)
 app.put('/authenticate', authenticate);
